6d865877e6e9f58049946e1616a82f11e0e6c846
matthewkirby <matthewkirby@email.arizona.edu>
2017-09-27 16:06:40 -0700

    Added new unit test and added 2nd article to the test file

--- tests/test_parsing_functions.py
+++ tests/test_parsing_functions.py
@@ -5,35 +5,65 @@ import parsing_functions as pf
 from bs4 import BeautifulSoup
 import pytest
 
-def test_load():
-    with open("test_call.txt") as fp:
-        soup = BeautifulSoup(fp)
-    test_load_title(soup)
-    test_load_authors(soup)
-    test_load_abstract(soup)
-    test_load_link(soup)
-    return
 
-@pytest.mark.skip
-def test_load_title(soup):
+def test_load_title():
+    with open("test_call.txt") as fp:
+        soup = BeautifulSoup(fp, 'xml')
     title = pf.load_title(soup)
     assert title == "Sample title with searchable keywords such as photons!"
     return
 
-@pytest.mark.skip
-def test_load_authors(soup):
+def test_load_authors():
+    with open("test_call.txt") as fp:
+        soup = BeautifulSoup(fp, 'xml')
     author_list = pf.load_authors(soup)
     assert author_list == ["Matthew Kirby", "Tom McClintock"]
     return
 
-@pytest.mark.skip
-def test_load_abstract(soup):
+def test_load_abstract():
+    with open("test_call.txt") as fp:
+        soup = BeautifulSoup(fp, 'xml')
     abstract = pf.load_abstract(soup)
-    assert abstract == "Something, something positions."
+    assert abstract == "Something, something positrons."
     return
 
-@pytest.mark.skip
-def test_load_link(soup):
+def test_load_link():
+    with open("test_call.txt") as fp:
+        soup = BeautifulSoup(fp, 'xml')
     link = pf.load_link(soup)
     assert link == "http://arxiv.org"
     return
+
+#def test_load_article():
+#    with open("test_call.txt") as fp:
+#        soup = BeautifulSoup(fp, 'xml')
+#    truth = pf.Article()
+#    truth.title = "Sample title with searchable keywords such as photons!"
+#    truth.authors = ["Matthew Kirby", "Tom McClintock"]
+#    truth.abstract = "Something, something positrons."
+#    truth.link = "http://arxiv.org"
+#    assert truth == load_article(soup)
+#    return
+
+def test_load_all_articles():
+    with open("test_call.txt") as fp:
+        soup = BeautifulSoup(fp, 'xml')
+    truth = pf.Article()
+    truth.title = "Sample title with searchable keywords such as photons!"
+    truth.authors = ["Matthew Kirby", "Tom McClintock"]
+    truth.abstract = "Something, something positrons."
+    truth.link = "http://arxiv.org"
+    truth2 = pf.Article()
+    truth2.title = "Hi there!"
+    truth2.authors = ["Erika Wagoner"]
+    truth2.abstract = "In a couple words"
+    truth2.link = "http://arxiv.org/isitnotanumberusually"
+    article_list = [truth, truth2]
+    assert article_list == pf.load_all_articles(soup)
+    return
+
+
+
+
+
+
