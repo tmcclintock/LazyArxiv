1371d6c65b3dabff9638db4e46c5472968baf69f
matthewkirby <matthewkirby@email.arizona.edu>
2017-09-27 17:04:58 -0700

    Functions written to parse the arxiv XML files into Article objects

--- parsing_functions.py
+++ parsing_functions.py
@@ -6,32 +6,48 @@ class Article():
         pass
 
     def __repr__(self):
-        pass
+        if len(self.authors) > 1:
+            return """%s\nAuthors: %s et al.\nAbstract: %s\n%s\n""" % (self.title, self.authors[0], self.abstract, self.link)
+        if len(self.authors) == 1:
+            return """%s\nAuthors: %s\nAbstract: %s\n%s\n""" % (self.title, self.authors[0], self.abstract, self.link)
 
-def load_title(soup):
-    pass
+def load_title(entry):
+    return entry.title.string
 
-def load_authors(soup):
-    pass
+def load_authors(entry):
+    author_list_dirty = entry.find_all('author')
+    author_list = [ad.find('name').string for ad in author_list_dirty]
+    return author_list
 
-def load_abstract(soup):
-    pass
+def load_abstract(entry):
+    return entry.summary.string.lstrip()
 
-def load_link(soup):
-    pass
+def load_link(entry):
+    return entry.id.string
 
+def load_article(entry):
+    article = Article()
+    article.title = load_title(entry)
+    article.authors = load_authors(entry)
+    article.abstract = load_abstract(entry)
+    article.link = load_link(entry)
+    return article
 
+def load_all_articles(soup):
+    entry_list = soup.find_all('entry')
+    article_list = [load_article(entry) for entry in entry_list]
+    return article_list
 
-
-
-
-
-
-
-
-
-
+def print_all_articles(alist):
+    for article in alist:
+        print article
+    return
 
 
 if __name__ == "__main__":
-    print "lol"
+    with open("tests/test_call.txt") as fp:
+        soup = BeautifulSoup(fp, 'xml')
+
+    article_list = load_all_articles(soup)
+    print_all_articles(article_list)
+
--- tests/test_parsing_functions.py
+++ tests/test_parsing_functions.py
@@ -9,61 +9,29 @@ import pytest
 def test_load_title():
     with open("test_call.txt") as fp:
         soup = BeautifulSoup(fp, 'xml')
-    title = pf.load_title(soup)
+    title = pf.load_title(soup.entry)
     assert title == "Sample title with searchable keywords such as photons!"
     return
 
 def test_load_authors():
     with open("test_call.txt") as fp:
         soup = BeautifulSoup(fp, 'xml')
-    author_list = pf.load_authors(soup)
+    author_list = pf.load_authors(soup.entry)
     assert author_list == ["Matthew Kirby", "Tom McClintock"]
     return
 
 def test_load_abstract():
     with open("test_call.txt") as fp:
         soup = BeautifulSoup(fp, 'xml')
-    abstract = pf.load_abstract(soup)
+    abstract = pf.load_abstract(soup.entry)
     assert abstract == "Something, something positrons."
     return
 
 def test_load_link():
     with open("test_call.txt") as fp:
         soup = BeautifulSoup(fp, 'xml')
-    link = pf.load_link(soup)
+    link = pf.load_link(soup.entry)
     assert link == "http://arxiv.org"
     return
-
-#def test_load_article():
-#    with open("test_call.txt") as fp:
-#        soup = BeautifulSoup(fp, 'xml')
-#    truth = pf.Article()
-#    truth.title = "Sample title with searchable keywords such as photons!"
-#    truth.authors = ["Matthew Kirby", "Tom McClintock"]
-#    truth.abstract = "Something, something positrons."
-#    truth.link = "http://arxiv.org"
-#    assert truth == load_article(soup)
-#    return
-
-def test_load_all_articles():
-    with open("test_call.txt") as fp:
-        soup = BeautifulSoup(fp, 'xml')
-    truth = pf.Article()
-    truth.title = "Sample title with searchable keywords such as photons!"
-    truth.authors = ["Matthew Kirby", "Tom McClintock"]
-    truth.abstract = "Something, something positrons."
-    truth.link = "http://arxiv.org"
-    truth2 = pf.Article()
-    truth2.title = "Hi there!"
-    truth2.authors = ["Erika Wagoner"]
-    truth2.abstract = "In a couple words"
-    truth2.link = "http://arxiv.org/isitnotanumberusually"
-    article_list = [truth, truth2]
-    assert article_list == pf.load_all_articles(soup)
-    return
-
-
-
-
 
 
